7013718418a765322079149f7ab8b075e3b00900
Bibhuti <bhusanniak2002@gmail.com>
2023-04-23 22:01:08 +0530

    changes made

--- SEM2/Assignment01/la1q5.java
+++ SEM2/Assignment01/la1q5.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class la1q5 
 {
     public static int sum_of_Digits(int n)
@@ -19,7 +21,18 @@ public class la1q5
         }
         return sum;
     }
-    public static void main(String[] args) {
-        System.out.println(sum_of_Digits(112439));
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number : ");int N = sc.nextInt();sc.close();
+        System.out.println("The sum of the digits of the number till it is a single digit number is : "+sum_of_Digits(N));
     }
+}
+
+class output_5
+{
+    /*
+    Enter a number : 124663
+    The sum of the digits of the number till it is a single digit number is : 4
+     */
 }
--- SEM2/Assignment01/la1q6.java
+++ SEM2/Assignment01/la1q6.java
@@ -10,15 +10,23 @@ public class la1q6
         sc.close();
     }
 
+    //method_1
     public static boolean isOdd_bit(int n)
     {
         return ((n & 1)==1);
     }
-
+    //method_2
     public static boolean isOdd_sub(int n)
     {   
         int i ;
         for ( i = n;i>0;i=i-2);
         return (i==1);
     }
+}
+
+class output_6
+{
+    /*
+    
+     */
 }
