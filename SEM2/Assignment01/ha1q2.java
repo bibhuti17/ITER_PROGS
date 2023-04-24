18df89671c9581af07a717f9ebd647a97539ec46
Bibhuti <bhusanniak2002@gmail.com>
2023-04-24 15:20:23 +0530

    some corrections

--- SEM2/Assignment01/ha1q2.java
+++ SEM2/Assignment01/ha1q2.java
@@ -4,16 +4,7 @@ public class ha1q2
 {
     public static void main(String[] args) 
     {
-        Scanner sc= new Scanner(System.in);
-        System.out.println("how many lines do you want to enter");
-        int n= sc.nextInt();
-        String [] str;
-        str= new String[n];
-        for(int i=0;i<n;i++)
-            str[i]=sc.nextLine();
-        for(int i=n;i>=0;i--)
-            System.out.println(str[i]);
-        sc.close();
+         
     }
 }
 
