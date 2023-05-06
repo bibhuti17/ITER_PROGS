33954759c6fcaaef022e44e5e7d18d36fddef3af
Bibhuti <bhusanniak2002@gmail.com>
2023-05-06 23:56:35 +0530

    java

--- SEM2/Assignment02/la2q2.java
+++ SEM2/Assignment02/la2q2.java
@@ -1,14 +1,30 @@
 package SEM2.Assignment02;
-
-public class la2q2 
-{
-    /*
-     * Question-2:
+ /*Question-2:
     Define a class called Complex with instance variables real, imag and instance methods
     setData(), display(), add(). Write a Java program to add two complex numbers.
     The prototype of add method is: 
-    public Complex add(Complex, Complex).
-     */
+    public Complex add(Complex, Complex).*/
+
+public class la2q2 
+{
+    public static void main(String[] args) {
+        Complex c1 = new Complex();
+        Complex c2 = new Complex();
+        Complex sum = new Complex();
+
+        c1.setData(3, 4);
+        c2.setData(5, 6);
+
+        System.out.print("Complex number 1: ");
+        c1.display();
+
+        System.out.print("Complex number 2: ");
+        c2.display();
+
+        sum = sum.add(c1, c2);
+        System.out.print("Sum of the two complex numbers: ");
+        sum.display();
+    }
 }
 
 class Complex {
@@ -34,23 +50,6 @@ class Complex {
         return sum;
     }
 
-    public static void main(String[] args) {
-        Complex c1 = new Complex();
-        Complex c2 = new Complex();
-        Complex sum = new Complex();
-
-        c1.setData(3, 4);
-        c2.setData(5, 6);
-
-        System.out.print("Complex number 1: ");
-        c1.display();
-
-        System.out.print("Complex number 2: ");
-        c2.display();
-
-        sum = sum.add(c1, c2);
-        System.out.print("Sum of the two complex numbers: ");
-        sum.display();
-    }
+    
 }
 
--- SEM2/Assignment02/la2q7.java
+++ SEM2/Assignment02/la2q7.java
@@ -1,8 +1,41 @@
 package SEM2.Assignment02;
 import java.util.*;
 
+
+
 public class la2q7{
     public static void main(String[] args) {
+        Employee obj = new Employee("Aman",24,64848,789000.36);
+        obj.empDisplay();
         
     }
 }
+
+class Person{
+    String name;
+    int age;
+
+    public Person(String name, int age){
+        this.name=name;
+        this.age=age;
+    }
+}
+
+class Employee extends Person{
+    int Eid;
+    double salary;
+
+    public Employee(String name,int age , int Eid , double salary){
+        super(name,age);
+        this.Eid=Eid;
+        this.salary=salary;
+    }
+
+        public void empDisplay(){
+            System.out.println("Employee name : "+name);
+            System.out.println("Employee's age : "+age);
+            System.out.println("Employee's id : "+Eid);
+            System.out.println("Employee's salary : "+salary);
+        }
+}
+
