4993e9a79ca80167f8f8976b549ae7c51c83022c
Bibhuti <bhusanniak2002@gmail.com>
2023-05-25 21:57:59 +0530

    java progs

--- SEM2/Assignment02/ha2q5.java
+++ SEM2/Assignment02/ha2q5.java
@@ -13,6 +13,37 @@ Sample Run:
 A “steak dinner” costing Rs. 600 is a “room service” provided 
 to room 1202 for Rs 24.00 fee.
 The total fee to pay is Rs 624. */
+import java.util.*;
 public class ha2q5 {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        RoomServiceMeal MS1 = new RoomServiceMeal();
+        MS1.display();
+
+        //RoomServiceMeal MS2 = new RoomServiceMeal();
+        //System.out.print("Enter the name of the food item : ");String name = sc.nextLine();
+        //System.out.print("Enter the price of the food item : ");double price = sc.nextDouble();sc.nextLine();
+        //System.out.print("Enter the name of the service : ");String Sname = sc.nextLine();
+        //System.out.print("Enter the service charges : ");double charges = sc.nextDouble();
+        //MS2.display();
+        sc.close();
+    }
+}
+interface RestaurantMeal{
+    String foodName = "steak dinner";
+    double foodPrice = 600;
+}
+interface HotelService{
+    String serviceName = "room service";
+    double serviceFee = 24;
+    int roomNo = 1202;
+} 
+
+class RoomServiceMeal implements RestaurantMeal,HotelService{
+    public RoomServiceMeal(){}
+     
+    public void display(){
+        System.out.println("Food ordered is "+foodName+" costing Rs."+foodPrice+"\n Service provided is "+serviceName+" charge is "+serviceFee);
+        System.out.println("The total bill to be paid is "+(foodPrice+serviceFee));
+    }
 }
